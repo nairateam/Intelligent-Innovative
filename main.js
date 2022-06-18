710ea63999ee4b29e206de618d541f046631566f
Oluwafemi. O <86085444+nairateam@users.noreply.github.com>
2022-06-18 11:45:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-// $(window).resize(function() {
-//     if($(window).width()<1020){
-//         $('.footer').removeClass('footer')
-//     }
-// });
+(function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          680: {
+            items: 2,
+            nav: false,
+            loop: false
+          },
+          1000: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
+  
